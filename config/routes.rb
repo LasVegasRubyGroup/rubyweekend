47a737187d294963c872202934b282dbb2a7e70c
Judd Lillestrand <juddl333@gmail.com>
2012-01-27 20:23:02 -0800

    removed dylans link

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rubyweekend::Application.routes.draw do
 
   resources :hidden_registrations
 
-  match '/dylan_registration', :to => 'hidden_registrations#new'
+  #match '/dylan_registration', :to => 'hidden_registrations#new'
 
   root :to => "pages#index"
   match '/resources', :to => 'pages#resources', :as => :resources
