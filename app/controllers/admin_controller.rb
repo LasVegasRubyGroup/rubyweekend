3bf1af33d117deb58d09ec6f4e2c869bf7c4bf0d
Bob Firestone <bob@rbf.me>
2012-04-20 15:47:12 -0700

    changed http_basic to environment variables

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with :name => "@lvrug", :password => "RubyWeekend$"
+  http_basic_authenticate_with :name => ENV["ADMIN_NAME"], :password => ENV["ADMIN_PASSWORD"]
 
   def admin
     @registered = Registration.all
