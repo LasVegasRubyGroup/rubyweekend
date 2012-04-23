d41df16d500bb786d026c92047f31dc3322289f8
Judd Lillestrand <juddl333@gmail.com>
2012-04-22 22:00:54 -0700

    login in dev, added a table

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,13 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with :name => ENV["ADMIN_NAME"], 
-  	:password => ENV["ADMIN_PASSWORD"]
+
+if Rails.env == "production"
+	http_basic_authenticate_with :name => ENV["ADMIN_NAME"], 
+  		:password => ENV["ADMIN_PASSWORD"]
+ else 
+ 	http_basic_authenticate_with :name => 'admin', 
+  		:password => 'password'
+ end
+  
 
   def admin
     @registered = Registration.ruby_weekend_2
