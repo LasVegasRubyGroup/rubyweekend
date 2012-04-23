9e54500b01d9560c4f1b3f6bd9b888d99dde54d7
Judd Lillestrand <juddl333@gmail.com>
2012-04-22 22:25:25 -0700

    added a clean up method for waiting lsit

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -1,4 +1,13 @@
 class Waitlist < ActiveRecord::Base
   validates_presence_of :name, :email
   validates_uniqueness_of :email
+
+  def clean_up
+  	registration_emails = Registration.all.map { |r| r.email }
+
+  	if registration_emails.include?(self.email)
+  		self.destroy
+  	end
+	end
+
 end
