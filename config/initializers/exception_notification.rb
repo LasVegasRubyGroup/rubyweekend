f77d57ff915680f5efbd98244b908fa82c729f30
Russ Smith <russ@bashme.org>
2012-08-10 16:34:38 -0700

    Oops.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,4 @@
-Phonecaseofthemonth::Application.config.middleware.use ExceptionNotifier,
+Rubyweekend::Application.config.middleware.use ExceptionNotifier,
   email_prefix: '[RubyWeekend Exception] ',
   sender_address: %{"notifier" <notifier@rubyweekend.com>},
   exception_recipients: %w{russ@bashme.org juddll3@gmail.com}
